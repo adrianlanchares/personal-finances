cb5e0b98f90d86452a6ee646ad66fceb813ab014
Adrian <cocoloco@Adrians-MacBook-Air.local>
2025-08-17 18:26:24 +0200

    create transaction admin

--- backend/transactions/admin.py
+++ backend/transactions/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Transaction
+
 
 # Register your models here.
+class TransactionAdmin(admin.ModelAdmin):
+    def has_add_permission(self, request):
+        return request.user.is_superuser
+
+    def has_change_permission(self, request, obj=None):
+        return request.user.is_superuser
+
+    def has_delete_permission(self, request, obj=None):
+        return request.user.is_superuser
+
+
+admin.site.register(Transaction, TransactionAdmin)
