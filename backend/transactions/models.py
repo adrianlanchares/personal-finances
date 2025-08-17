a21e063a76e653a4cbcf87f41e3f2f2699043bfc
Adrian <cocoloco@Adrians-MacBook-Air.local>
2025-08-17 18:11:00 +0200

    create transactions model

--- backend/transactions/models.py
+++ backend/transactions/models.py
@@ -2,3 +2,23 @@ from django.db import models
 
 
 # Create your models here.
+class Transaction(models.Model):
+    """Represents a financial transaction.
+
+    Args:
+        models (Model): Django model base class.
+    """
+
+    id = models.AutoField(primary_key=True)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.CharField(max_length=255)
+    category = models.CharField(max_length=100)
+    account = models.CharField(max_length=100)
+    cashflow = models.CharField(max_length=100)
+    datetime = models.DateTimeField()
+
+    def __str__(self):
+        return f"Transaction {self.id}: {self.description} - {self.amount}"
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
