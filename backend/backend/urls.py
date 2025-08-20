1a6b299d9fd5ab00ae034bcdbafb972b3b6ee2c1
Adrian <cocoloco@Adrians-MacBook-Air.local>
2025-08-20 16:50:43 +0200

    fix transactions import path

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -18,7 +18,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from ..transactions import views
+from backend.transactions import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
