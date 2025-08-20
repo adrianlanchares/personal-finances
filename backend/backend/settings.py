9eb4603be69c7b8f906442aef811b9cbe59c171c
Adrian <cocoloco@Adrians-MacBook-Air.local>
2025-08-20 18:47:49 +0200

    allow mac host

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DJANGO_DEBUG")
 
-ALLOWED_HOSTS = ["10.8.0.1"]
+ALLOWED_HOSTS = ["10.8.0.1", "10.8.0.2"]
 
 
 # Application definition
