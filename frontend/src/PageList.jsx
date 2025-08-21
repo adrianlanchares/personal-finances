a0fa68e22884cb799d7ff638c635a12dd04bca35
coco <adrianlopezlanchares@gmail.com>
2025-08-21 12:41:16 +0200

    add cashflow filter

--- frontend/src/PageList.jsx
+++ frontend/src/PageList.jsx
@@ -96,6 +96,22 @@ function AccountFilter({account, setAccount}) {
   );
 }
 
+function CashflowFilter({cashflow, setCashflow}) {
+  return (
+    <div className="CashflowFilter">
+      <p>
+        <strong>Cashflow<br/></strong>
+        <input type="text" value={cashflow} onChange={(e) => setCashflow(e.target.value)} placeholder="Cashflow"/>
+      </p>
+    </div>
+  );
+}
+
+      </p>
+    </div>
+  );
+}
+
 
 function TransactionList({transactionList}) {
   return (<div>
