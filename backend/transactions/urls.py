417d513d51e80155df033782d4d067f0c3e9b61e
coco <adrianlopezlanchares@gmail.com>
2025-08-21 20:36:29 +0200

    add url to account balances

--- backend/transactions/urls.py
+++ backend/transactions/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("", views.ListTransactionsView.as_view(), name="transactions"),
     path("create/", views.CreateTransactionView.as_view(), name="create-transaction"),
     path("<int:pk>/", views.TransactionView.as_view(), name="transaction-details"),
+    path("balance/", views.AccountBalancesView.as_view(), name="account-balances"),
 ]
