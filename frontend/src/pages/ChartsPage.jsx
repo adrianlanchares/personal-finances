59bf4de3242bfd4c8d517d3e62f36e693ce575bc
coco <adrianlopezlanchares@gmail.com>
2025-08-23 19:20:59 +0200

    add useEffect and useState imports

--- frontend/src/pages/ChartsPage.jsx
+++ frontend/src/pages/ChartsPage.jsx
@@ -1,9 +1,10 @@
+import { useEffect, useState } from 'react';
 import CategoryPieChart from "../components/charts/CategoryPieChart.jsx";
 
 export default function ChartsPage(){
-    const [transactionList, setTransactionList] = React.useState([]);
+    const [transactionList, setTransactionList] = useState([]);
 
-    React.useEffect(() => {
+    useEffect(() => {
         const fetchData = async () => {
             const response = await fetch("http://10.8.0.1:8000/transactions/");
             const data = await response.json();
