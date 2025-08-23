542a77f462a25ea7971ce8fc5b7bb0cc9a42ac57
coco <adrianlopezlanchares@gmail.com>
2025-08-23 19:52:27 +0200

    change state names

--- frontend/src/components/filters/AmountFilters.jsx
+++ frontend/src/components/filters/AmountFilters.jsx
@@ -1,20 +1,20 @@
-export function MinAmountFilter({amount, setAmount}) {
+export function MinAmountFilter({minAmount, setMinAmount}) {
   return (
     <div className="MinAmountFilter">
       <p>
         <strong>Min Amount<br/></strong>
-        <input type="number" value={amount} onChange={e => setAmount(Math.max(0, e.target.value))} placeholder="Min Amount"/>
+        <input type="number" value={minAmount} onChange={e => setMinAmount(Math.max(0, e.target.value))} placeholder="Min Amount"/>
       </p>
     </div>
   );
 }
 
-export function MaxAmountFilter({amount, setAmount}) {
+export function MaxAmountFilter({maxAmount, setMaxAmount}) {
   return (
     <div className="MaxAmountFilter">
       <p>
         <strong>Max Amount<br/></strong>
-        <input type="number" value={amount} onChange={e => setAmount(Math.max(0, e.target.value))} placeholder="Max Amount"/>
+        <input type="number" value={maxAmount} onChange={e => setMaxAmount(Math.max(0, e.target.value))} placeholder="Max Amount"/>
       </p>
     </div>
   );
