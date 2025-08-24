31928d8b97957433952e084374edaaa51f877346
coco <adrianlopezlanchares@gmail.com>
2025-08-24 21:09:49 +0200

    make filter select from menu

--- frontend/src/components/filters/CashflowFilter.jsx
+++ frontend/src/components/filters/CashflowFilter.jsx
@@ -1,10 +1,19 @@
-export default function CashflowFilter({cashflow, setCashflow}) {
+import { Select, MenuItem, FormControl, InputLabel } from "@mui/material";
+
+export default function CashflowFilter({ cashflow, setCashflow }) {
   return (
-    <div className="CashflowFilter">
-      <p>
-        <strong>Cashflow<br/></strong>
-        <input type="text" value={cashflow} onChange={(e) => setCashflow(e.target.value)} placeholder="Cashflow"/>
-      </p>
-    </div>
+    <FormControl fullWidth>
+      <InputLabel>Cashflow</InputLabel>
+      <Select
+        defaultValue="all"
+        value={cashflow}
+        label="Cashflow"
+        onChange={(e) => setCashflow(e.target.value)}
+      >
+        <MenuItem value="">All</MenuItem>
+        <MenuItem value="income">Income</MenuItem>
+        <MenuItem value="expense">Expense</MenuItem>
+      </Select>
+    </FormControl>
   );
 }
