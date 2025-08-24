4c7e0c9405d99145e933897fb604f782f1d9a826
coco <adrianlopezlanchares@gmail.com>
2025-08-24 22:40:44 +0200

    add responsive container

--- frontend/src/components/charts/CategoryPieChart.jsx
+++ frontend/src/components/charts/CategoryPieChart.jsx
@@ -1,5 +1,12 @@
 import React, { useMemo } from "react";
-import { PieChart, Pie, Cell, Tooltip, Legend } from "recharts";
+import {
+    PieChart,
+    Pie,
+    Cell,
+    Tooltip,
+    Legend,
+    ResponsiveContainer,
+} from "recharts";
 
 /// Define some colors
 const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042", "#AA336A"];
@@ -21,26 +28,28 @@ export default function CategoryPieChart({ transactions }) {
     }, [transactions]);
 
     return (
-        <PieChart width={500} height={500}>
-            <Pie
-                data={data}
-                dataKey="value"
-                nameKey="name"
-                cx="50%"
-                cy="50%"
-                outerRadius={150}
-                label
-                isAnimationActive={false}
-            >
-                {data.map((entry, index) => (
-                    <Cell
-                        key={`cell-${index}`}
-                        fill={COLORS[index % COLORS.length]}
-                    />
-                ))}
-            </Pie>
-            <Tooltip />
-            <Legend />
-        </PieChart>
+        <ResponsiveContainer width="100%" height={400}>
+            <PieChart>
+                <Pie
+                    data={data}
+                    dataKey="value"
+                    nameKey="name"
+                    cx="50%"
+                    cy="50%"
+                    outerRadius={150}
+                    label
+                    isAnimationActive={false}
+                >
+                    {data.map((entry, index) => (
+                        <Cell
+                            key={`cell-${index}`}
+                            fill={COLORS[index % COLORS.length]}
+                        />
+                    ))}
+                </Pie>
+                <Tooltip />
+                <Legend />
+            </PieChart>
+        </ResponsiveContainer>
     );
 }
